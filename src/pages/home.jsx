4c76d5d9529ac74f0cb4d7ff39f8c70e5e069ddf
Mrinal <paulmrinal12@gmail.com>
2025-03-22 16:35:01 +0530

    Updated project

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import Input from "../components/Inputs/Input";
 import Footer from "../components/Footer/Footer";
-import "./Home.css";
+import "./home.css";
 
 const Home = ({ setUserData }) => {
   return (
